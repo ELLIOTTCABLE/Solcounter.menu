2144a9dd19868938082f328cb11891ac514f309f
elliottcable <me@ell.io>
2010-06-07 22:40:35 -0800

    Adding a Preference Pane

---
+++ Preference Pane/SolcounterPref.h
@@ -0,0 +1,19 @@
+//
+//  SolcounterPref.h
+//  Solcounter
+//
+//  Created by Elliott Cable on 6/7.
+//  Copyright (c) 2010 yreality. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface SolcounterPref : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ Preference Pane/SolcounterPref.m
@@ -0,0 +1,18 @@
+//
+//  SolcounterPref.m
+//  Solcounter
+//
+//  Created by Elliott Cable on 6/7.
+//  Copyright (c) 2010 yreality. All rights reserved.
+//
+
+#import "SolcounterPref.h"
+
+
+@implementation SolcounterPref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
